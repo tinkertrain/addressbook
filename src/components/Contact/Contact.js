1bc12bb4b1c4f5b5635f97c067a39d8eea22b989
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-03-16 14:28:25 -0600

    Adds contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Gravatar } from '../Gravatar/Gravatar';
+import './Contact.scss';
+
+export class Contact extends Component {
+  render() {
+    let { firstname, lastname, email, clickHandler } = this.props;
+
+    return (
+      <li className="ContactItem" onClick={clickHandler}>
+        <div className="ContactItem__Gravatar">
+          <Gravatar email={email} />
+        </div>
+        <div className="ContactItem__FullName">
+          <span className="FullName__First">{ firstname }</span>
+          <span className="FullName__Last">{ lastname }</span>
+        </div>
+      </li>
+    );
+  }
+}
