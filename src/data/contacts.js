43369d886ea1099945895a40498f2174c1095ad0
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-03-17 16:23:00 -0600

    get contacts from localstorage or initialize with empty array

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -1,12 +1,23 @@
-let contacts = [
-  {
-    id: 0,
-    firstname: 'Juan Pablo',
-    lastname: 'Lomeli Diaz',
-    email: 'test@gmail.com',
-    country: 'MX'
-  }
-];
+import Lockr from 'lockr';
+
+// let contacts = [
+//   {
+//     id: 0,
+//     firstname: 'Juan Pablo',
+//     lastname: 'Lomeli Diaz',
+//     email: 'tinkertrain@gmail.com',
+//     country: 'MX'
+//   },
+//   {
+//     id: 1,
+//     firstname: 'Christopher',
+//     lastname: 'Walken',
+//     email: 'test@abc.com',
+//     country: 'US'
+//   }
+// ];
+
+let contacts = Lockr.get('contacts') || [];
 
 export {
   contacts
