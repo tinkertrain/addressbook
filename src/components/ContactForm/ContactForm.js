18d51e20440626a57bb416db2c1503a41d75ce75
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2016-03-17 16:23:21 -0600

    change value for defaultvalue

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import countries from 'country-list/data.json';
 import { validateEmail } from '../../utils/utils';
 
+import './ContactForm.scss';
+
 export class ContactForm extends Component {
   state = {
     errors: {}
@@ -30,33 +32,35 @@ export class ContactForm extends Component {
               <button className="button button--warning" onClick={handleCancelClick}>Cancel</button>
             </div>
           </header>
-          <div className="ContactForm__Input">
-            {this.state.errors.firstname ? (<div className="Error">Please enter a first name</div>) : null}
-            <label htmlFor="firstname">First Name</label>
-            <input ref={(c) => this._firstname = c} type="text" id="firstname" value={firstname}/>
-          </div>
-          <div className="ContactForm__Input">
-            {this.state.errors.lastname ? (<div className="Error">Please enter a last name</div>) : null}
-            <label htmlFor="lastname">Last Name</label>
-            <input ref={(c) => this._lastname = c} type="text" id="lastname" value={lastname}/>
-          </div>
-          <div className="ContactForm__Input">
-            {this.state.errors.email ? (<div className="Error">Please enter a valid email</div>) : null}
-            <label htmlFor="email">Email</label>
-            <input ref={(c) => this._email = c} type="email" id="email" value={email}/>
-          </div>
-          <div className="ContactForm__Input">
-            <label htmlFor="country">Country</label>
-            <select ref={(c) => this._country = c} id="country" value={country}>
-              {
-                countries.map((c) => (
-                  <option
-                    key={c.code}
-                    value={c.code}
-                  >{c.name}</option>
-                ))
-              }
-            </select>
+          <div className="ContactForm__Body">
+            <div className="ContactForm__Input">
+              {this.state.errors.firstname ? (<div className="Error">Please enter a first name</div>) : null}
+              <label htmlFor="firstname">First Name</label>
+              <input ref={(c) => this._firstname = c} type="text" id="firstname" defaultValue={firstname}/>
+            </div>
+            <div className="ContactForm__Input">
+              {this.state.errors.lastname ? (<div className="Error">Please enter a last name</div>) : null}
+              <label htmlFor="lastname">Last Name</label>
+              <input ref={(c) => this._lastname = c} type="text" id="lastname" defaultValue={lastname}/>
+            </div>
+            <div className="ContactForm__Input">
+              {this.state.errors.email ? (<div className="Error">Please enter a valid email</div>) : null}
+              <label htmlFor="email">Email</label>
+              <input ref={(c) => this._email = c} type="email" id="email" defaultValue={email}/>
+            </div>
+            <div className="ContactForm__Input">
+              <label htmlFor="country">Country</label>
+              <select ref={(c) => this._country = c} id="country" defaultValue={country}>
+                {
+                  countries.map((c) => (
+                    <option
+                      key={c.code}
+                      value={c.code}
+                    >{c.name}</option>
+                  ))
+                }
+              </select>
+            </div>
           </div>
         </form>
       </div>
@@ -69,7 +73,8 @@ export class ContactForm extends Component {
 
   handleSubmitForm(e) {
     e.preventDefault();
-    let { saveUser } = this.props;
+
+    let { saveUser, updateUser, mode } = this.props;
     let errors = {};
     let valid = true;
 
@@ -97,8 +102,12 @@ export class ContactForm extends Component {
       this.setState({ errors });
     }
     else {
-      saveUser(newUser);
+      if (mode === 'Adding New Contact') {
+        saveUser(newUser);
+      }
+      else {
+        updateUser(this.props.data.id, newUser);
+      }
     }
-
   }
 }
